58c16cc77bdf909c429584d34a60798fefcbf5e0
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-06 22:38:11 +0200

    "adding routes to server"

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
 const app = express();
@@ -17,3 +18,13 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
+
+// Routes
+app.use('/api', taskRoutes);
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
